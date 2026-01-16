f5eb8570e255ef39fcca175867a50a3ea91187a0
Aleksa Milic <baba12milic@gmail.com>
2026-01-16 10:18:14 +0100

    Color changing waves.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const w0 = document.querySelector('.w0');
+const w1 = document.querySelector('.w1');
+const w2 = document.querySelector('.w2');
+
 const animateWave = (wave, duration) => {
   const tl = gsap.timeline({ repeat: -1 });
   tl.to(wave, {
@@ -9,6 +13,18 @@ const animateWave = (wave, duration) => {
   });
 };
 
-animateWave('.w0', 5);
+animateWave(w0, 5);
 animateWave('.w1', 8);
 animateWave('.w2', 12);
+
+let hueR = 0;
+
+const hueRotate = () => {
+  hueR++;
+
+  w0.style.filter = `hue-rotate(${hueR}deg)`;
+  w1.style.filter = `hue-rotate(${hueR + 10}deg)`;
+  w2.style.filter = `hue-rotate(${hueR + 20}deg)`;
+};
+
+setInterval(hueRotate, 100);
