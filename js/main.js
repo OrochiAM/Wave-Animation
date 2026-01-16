1344c7ca91a30fef33fcd96ec1367c7edb3da478
Aleksa Milic <baba12milic@gmail.com>
2026-01-16 10:06:47 +0100

    More waves.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-const tl = gsap.timeline({ repeat: -1 });
+const animateWave = (wave, duration) => {
+  const tl = gsap.timeline({ repeat: -1 });
+  tl.to(wave, {
+    xPercent: '+=50',
+    duration: duration,
+    ease: 'none',
+  }).set('img', {
+    xPercent: '-=50',
+  });
+};
 
-tl.to('img', {
-  xPercent: '+=50',
-  duration: 5,
-  ease: 'none',
-}).set('img', {
-  xPercent: '-=50',
-});
+animateWave('.w0', 5);
+animateWave('.w1', 8);
+animateWave('.w2', 12);
